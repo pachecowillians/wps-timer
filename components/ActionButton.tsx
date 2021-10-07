95fe74e2ccd06c91ae6840c1f8ea8d7cd45b002e
Willian Pacheco <willianpacheco31@gmail.com>
2021-10-07 16:23:14 -0300

    Implement collapse funcionality

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import styles from '../styles/ActionButton.module.css'
 import TimeOption from './TimeOption'
 
@@ -10,21 +10,38 @@ type ActionButtonProps = {
 }
 
 const ActionButton: React.FC<ActionButtonProps> = ({ start, stop, active, setTime }) => {
+    const [opened, setOpened] = useState(false);
+
+    function buttonAction() {
+        if (active) {
+            stop();
+        } else {
+            if (opened) {
+                close();
+            } else {
+                open();
+            }
+        }
+    }
+
+    const close = useCallback(() => { setOpened(false); }, [],);
+    const open = useCallback(() => { setOpened(true); }, [],);
+
     return (
         <div className={styles.container}>
             <div className={styles.buttonContainer}>
-                <button onClick={active ? stop : start} className={active ? styles.active : styles.notActive}>
+                <button onClick={buttonAction} className={active ? styles.active : styles.notActive}>
                     {/* <span>{!active ? '▶' : '🗙'}</span> */}
                     <div className={!active ? styles.start : styles.stop} />
                 </button>
 
             </div>
             <div className={styles.timeOptions}>
-                <TimeOption time={5} setTime={setTime} start={start} />
-                <TimeOption time={10} setTime={setTime} start={start} />
-                <TimeOption time={25} setTime={setTime} start={start} />
-                <TimeOption time={30} setTime={setTime} start={start} />
-                <TimeOption time={45} setTime={setTime} start={start} />
+                <TimeOption time={5} setTime={setTime} start={start} opened={opened} close={close} />
+                <TimeOption time={10} setTime={setTime} start={start} opened={opened} close={close} />
+                <TimeOption time={25} setTime={setTime} start={start} opened={opened} close={close} />
+                <TimeOption time={30} setTime={setTime} start={start} opened={opened} close={close} />
+                <TimeOption time={45} setTime={setTime} start={start} opened={opened} close={close} />
             </div>
         </div>
     )
--- components/TimeOption.tsx
+++ components/TimeOption.tsx
@@ -3,19 +3,23 @@ import styles from '../styles/TimeOption.module.css'
 
 type TimeOptionProps = {
     time: number;
+    opened: boolean;
     setTime: (time: number) => void;
     start: () => void;
+    close: () => void;
 }
 
-const TimeOption: React.FC<TimeOptionProps> = ({ time, setTime, start }) => {
+const TimeOption: React.FC<TimeOptionProps> = ({ time, setTime, start, opened, close }) => {
+    
     function startClock() {
         setTime(time * 60);
+        close();
         start();
     }
 
     return (
         <>
-            <div className={styles.container} onClick={startClock}>
+            <div className={styles.container} onClick={startClock} style={!opened ? { opacity: 0 } : { opacity: 1 }}>
                 {time} min
             </div>
         </>
