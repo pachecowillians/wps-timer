6aa1b493849113d4d92e385a1c182605e54e66eb
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-27 23:03:39 -0300

    Converting TimerButton into button

--- components/TimerButton/TimerButton.tsx
+++ components/TimerButton/TimerButton.tsx
@@ -1,9 +1,9 @@
 import styles from './styles.module.css'
 
 export default function TimerButton() {
-    return <div className={styles.container}>
+    return <button className={styles.container}>
         <span className="material-symbols-outlined">
             play_arrow
         </span>
-    </div>
+    </button>
 }
